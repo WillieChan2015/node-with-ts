625848ef2d41dea8064a30548b22f6a3587a8783
Willie Chen <willie.chen@jodoinc.com>
2019-07-03 17:18:28 +0800

    update:增加日志配置及输出到文件

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,14 +1,54 @@
 import log4js from 'log4js';
 import koa from 'koa';
 
-const logger = log4js.getLogger();
-logger.level = 'debug';
+const logLevel = process.env.NODE_ENV === 'production' ? "info" : "debug";
+
+log4js.configure({
+    appenders: {
+        console: { type: 'stdout' },
+        normal: {
+            type: 'dateFile', 
+            filename: 'logs/log',
+            maxLogSize: 1024*1024*10, // 只有type为file时生效
+            backups: 10,    // 只有type为file时生效，表示超过10个log文件时，删除旧的，保留最新的10个
+            category: "dateFileLog",
+            pattern: "_yyyy-MM-dd.log",
+            alwaysIncludePattern: true,
+            layout: {
+                type: 'pattern',
+                pattern: '%d{yyyy-MM-dd hh:mm:ss} [%p] %c - %m%n'
+            }
+        },
+        log_error: {
+            type: "file",  
+            filename: "logs/error.log",
+            maxLogSize: 1024*1024*100, // 只有type为file时生效
+            layout: {
+                type: 'pattern',
+                pattern: '%d{yyyy-MM-dd hh:mm:ss} [%p] %c - %m%n'
+            }
+        },
+        error: {
+            type: 'logLevelFilter',
+            level: 'warn',
+            appender: 'log_error',
+        }
+    },
+    categories: {
+        default: { appenders: ['console', 'normal', 'error'], level: logLevel },
+    },
+});
 
 export async function loggerMiddleware(ctx: koa.ParameterizedContext, next: () => Promise<any>): Promise<any> {
+    const logger = log4js.getLogger();
     logger.info('method: ' + ctx.method);
     logger.info('request path: ' + ctx.path);
-    console.log();
     return next();
 }
 
-export default logger;
+export function log(name: string) {
+    const logger = log4js.getLogger(name);
+    return logger;
+}
+
+export default log4js.getLogger();
